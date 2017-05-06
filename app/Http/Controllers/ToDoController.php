5dc5ee50cd5decb53bb12f32ac0c419872ba5048
Suzal3579 <pe.messh@gmail.com>
2017-05-06 23:48:01 +0545

    Mind change .. uploading entire framework ...

--- app/Http/Controllers/ToDoController.php
+++ app/Http/Controllers/ToDoController.php
@@ -23,7 +23,7 @@ class ToDoController extends Controller
     */
     public function create()
     {
-        //
+        return view("toravel.create");
     }
     
     /**
---
+++ resources/views/toravel/create.blade.php
@@ -0,0 +1,7 @@
+@extends("layout.layout")
+
+@section("title","Create")
+
+@section("body")
+
+@endsection
